8ef629f5654849498b477ad8f6d4a364fc351803
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-05-09 16:41:13 +0100

    fix main

--- similarity_search_service/app/main.py
+++ similarity_search_service/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import embeddings 
-from app.api.v1.endpoints import anomalydetection
 from app.config.qdrant_config import initialize_qdrant_collection
 
 app = FastAPI()
