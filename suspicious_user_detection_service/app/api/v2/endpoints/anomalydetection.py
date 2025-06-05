d569b9e59288807450aca23f84c5b61bd08d79e8
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-06-05 12:57:09 +0100

    first step in linking redis

--- suspicious_user_detection_service/app/api/v2/endpoints/anomalydetection.py
+++ suspicious_user_detection_service/app/api/v2/endpoints/anomalydetection.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, File, Form, UploadFile
 from app.models.anomaly import userPosts, AnomalyResponse
 from app.services.scam_detector_agent import scam_detector_agent
+from app.config.redis_client import redis_client, test_redis
 
 router = APIRouter()
 
@@ -14,3 +15,11 @@ async def detect_suspicious_user(
 
 
     return {"message": "Suspicious user detected"}
+
+
+@router.get("/get/")
+async def get_key():
+    try:
+        return await test_redis()
+    except Exception as e:
+        return {"error": str(e)}
