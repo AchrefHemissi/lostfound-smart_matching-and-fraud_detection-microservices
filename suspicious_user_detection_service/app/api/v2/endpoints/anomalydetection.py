3d053664e8e5fbedd09499811a93dadc3d962a62
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-06-05 15:23:54 +0100

    add the repository of redis

--- suspicious_user_detection_service/app/api/v2/endpoints/anomalydetection.py
+++ suspicious_user_detection_service/app/api/v2/endpoints/anomalydetection.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, File, Form, UploadFile
 from app.models.anomaly import userPosts, AnomalyResponse
 from app.services.scam_detector_agent import scam_detector_agent
 from app.config.redis_client import redis_client, test_redis
+from app.repositories.redis_repo import test
 
 router = APIRouter()
 
@@ -17,9 +18,16 @@ async def detect_suspicious_user(
     return {"message": "Suspicious user detected"}
 
 
-@router.get("/get/")
+@router.get("/test_redis/")
 async def get_key():
     try:
         return await test_redis()
+    except Exception as e:
+        return {"error": str(e)}
+
+@router.get("/test_repo/")
+async def test_repo():
+    try:
+        await test()
     except Exception as e:
         return {"error": str(e)}
